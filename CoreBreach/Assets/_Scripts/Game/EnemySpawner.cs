9cfd0c9006c5546b022fc8beafe4104f749740d4
fratyaldiz <firatylz2004@hotmail.com>
2026-05-16 21:27:10 +0300

    feat: game state manager with win/lose, fix pickup detection

--- CoreBreach/Assets/_Scripts/Game/EnemySpawner.cs
+++ CoreBreach/Assets/_Scripts/Game/EnemySpawner.cs
@@ -9,13 +9,13 @@ public class EnemySpawner : MonoBehaviour
     [SerializeField] private int enemiesPerWave =5;
     [SerializeField] private float timeBetweenSpawns = 1.5f;
     [SerializeField] private float timeBetweenWaves = 5f;
-    [SerializeField] private int totalWaves =3;
+    [SerializeField] private int totalWaves=3;
 
     private int currentWave= 0;
 
     private void Start()
     {
-        if (enemyPool == null|| spawnPoints== null|| spawnPoints.Length== 0)
+        if (enemyPool == null|| spawnPoints== null|| spawnPoints.Length==0)
         {
             Debug.LogError("EnemySpawner setup is broken");
             return;
@@ -24,30 +24,35 @@ public class EnemySpawner : MonoBehaviour
         StartCoroutine(SpawnWaves());
     }
 
-    private IEnumerator SpawnWaves()
+private IEnumerator SpawnWaves()
+{
+    yield return new WaitForSeconds(2f);
+
+    while (currentWave< totalWaves)
     {
-        // wait a bit so player can prepare
-        yield return new WaitForSeconds(2f);
+        currentWave++;
+        Debug.Log("Wave "+ currentWave +" start!");
 
-        while (currentWave < totalWaves)
+        for (int i = 0; i < enemiesPerWave; i++)
         {
-            currentWave++;
-            Debug.Log("Wave " +currentWave + "start!");
-
-            for (int i = 0; i < enemiesPerWave; i++)
-            {
-                SpawnOneEnemy();
-                yield return new WaitForSeconds(timeBetweenSpawns);
-            }
-
-            GameEvents.RaiseWaveCompleted(currentWave);
-            Debug.Log("Wave " + currentWave +" done!");
-
-            yield return new WaitForSeconds(timeBetweenWaves);
+            SpawnOneEnemy();
+            yield return new WaitForSeconds(timeBetweenSpawns);
         }
 
-        Debug.Log("All waves done, player win!");
+        GameEvents.RaiseWaveCompleted(currentWave);
+        Debug.Log("Wave "+currentWave+" done!");
+
+        yield return new WaitForSeconds(timeBetweenWaves);
     }
+
+    Debug.Log("All waves done, player win!");
+
+    // tell game state manager we won
+    if (GameStateManager.Instance!= null)
+    {
+        GameStateManager.Instance.DeclareVictory();
+    }
+}
 
     private void SpawnOneEnemy()
     {
--- CoreBreach/Assets/_Scripts/Weapons/WeaponPickup.cs
+++ CoreBreach/Assets/_Scripts/Weapons/WeaponPickup.cs
@@ -15,20 +15,17 @@ public class WeaponPickup : MonoBehaviour
 
     private void Update()
     {
-        transform.Rotate(0,rotateSpeed *Time.deltaTime,0);
+        transform.Rotate(0,rotateSpeed *Time.deltaTime, 0);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // try to find Weapon in this object, parent, or children
-        Weapon weapon =other.GetComponent<Weapon>();
-        if (weapon== null) weapon = other.GetComponentInParent<Weapon>();
-        if (weapon ==null) weapon = other.GetComponentInChildren<Weapon>();
+        Weapon weapon = FindFirstObjectByType<Weapon>();
 
         if (weapon != null)
         {
             weapon.ApplyDecorator(type);
-            Debug.Log("Pickup taken: "+type);
+            Debug.Log("Pickup taken: " +type);
             Destroy(gameObject);
         }
     }
